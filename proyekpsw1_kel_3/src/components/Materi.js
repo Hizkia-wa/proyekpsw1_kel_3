dea543b7e38f59c151619f94b5c627e855e59702
Hizkia-wa <siahaanhizkia06@gmail.com>
2024-11-16 13:17:18 +0700

    mebuat file baru dan halaman LatihamSoal.js

--- proyekpsw1_kel_3/src/components/Materi.js
+++ proyekpsw1_kel_3/src/components/Materi.js
@@ -1,14 +1,14 @@
 import React from "react";
 import './Materi.css';
-import gbdimages from '../images/gbd.Png';
-import gbrimages from '../images/gbr.png';
+import gbdsimages from '../images/gbds.Png';
+import gbrsimages from '../images/gbrs.png';
 import stkimages from '../images/stk.jpg';
 import pkimages from '../images/pk.jpg';
 
 function Materi() {
     const jurusanData = [
-        {nama: 'Goometri Bangun Datar', deskripsi: '//', gambar: gbdimages},
-        {nama: 'Goometri Bangun Ruang', deskripsi: '//', gambar: gbrimages},
+        {nama: 'Goometri Bangun Datar', deskripsi: '//', gambar: gbdsimages},
+        {nama: 'Goometri Bangun Ruang', deskripsi: '//', gambar: gbrsimages},
         {nama: 'Statistika', deskripsi: '//', gambar: stkimages},
         {nama: 'Peluang Kejadian', deskripsi: '//', gambar:pkimages},
     ];
