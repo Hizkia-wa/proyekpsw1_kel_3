71c064d6519f891f24aa3e9686f91571d80ac882
Nathan-0011 <jonathansimamoraa02@gmail.com>
2024-11-19 14:26:43 +0700

    Memperbarui style dari Navbar Dan memperbaiki sedikit

--- proyekpsw1_kel_3/src/components/Navbar.js
+++ proyekpsw1_kel_3/src/components/Navbar.js
@@ -1,21 +1,26 @@
 import React from "react";
-import './Navbar.css';
-import { Link } from 'react-router-dom';
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import {FaHome,FaBook,FaQuestionCircle,FaPuzzlePiece,FaStar,} from "react-icons/fa";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <div className="navbar-links">
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/materi">Materi</Link></li>
-                <li><Link to="/latihansoal">Latihan Soal</Link></li>
-                <li><Link to="/soallatihan">Soal Latihan</Link></li>
-                <li><Link to="/brilian">Brilian</Link></li>
-            </ul>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <h2>R U M A P I</h2>
+      </div>
+      <div className="navbar-links">
+        <ul>
+          <li><Link to="/"><FaHome /> Home</Link></li>
+          <li><Link to="/materi"><FaBook /> Materi</Link></li>
+          <li><Link to="/latihansoal"><FaQuestionCircle /> Latihan Soal</Link></li>
+          <li><Link to="/soallatihan"><FaPuzzlePiece /> Soal Latihan</Link></li>
+          <li><Link to="/brilian"><FaStar /> Brilian</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
