887b8bf5fe34223643b977804cfc87d9aa4e6e5f
Hizkia-wa <siahaanhizkia06@gmail.com>
2024-12-03 16:05:48 +0700

    memperbaiki error dan memperbaharui LatihanGeometri

--- proyekpsw1_kel_3/src/App.js
+++ proyekpsw1_kel_3/src/App.js
@@ -45,11 +45,7 @@ import BrilianTurunan from "./components/BrilianTurunan";
 import BrilianKongruen from "./components/BrilianKongruen";
 import SoalLatihanKesebangunan from "./components/SoalLatihanKesebangunan";
 import SoalLatihanLimit from "./components/SoalLatihanLimit";
-<<<<<<< HEAD
-import SoalLatihanBinomial from "./components/SoalLatihanBinomial.js";
-=======
-import SoallatihanBinomial from "./components/SoallatihanBinomial";
->>>>>>> 062d21dc3472a7af1f94646eaf651fc9200cc905
+import SoalLatihanBinomial from "./components/SoalLatihanBinomial";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function CenteredLayout({ children }) {
@@ -133,12 +129,8 @@ function App() {
           <Route path="/latihansoalkesebangunan"  element={<LatihanSoalKesebangunan />} />
           <Route path="/Soallatihankesebangunan" element={<SoalLatihanKesebangunan />} />
           <Route path="/Soallatihanlimit" element={<SoalLatihanLimit />} />
-<<<<<<< HEAD
           <Route path="/Soallatihanbinomial" element={<SoalLatihanBinomial />} />
-=======
-          <Route path="/SoallatihanBinomial" element={< SoallatihanBinomial />} />
 
->>>>>>> 062d21dc3472a7af1f94646eaf651fc9200cc905
 
           <Route
             path="*"
